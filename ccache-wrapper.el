01a36c54f2f1bf5d8486fa4bcae38fdec4a378df
Fu Zhengyi <tsingyat@outlook.com>
2023-04-04 23:38:28 +0800

    Change ccache-wrapper-mode to a local minor mode

--- ccache-wrapper.el
+++ ccache-wrapper.el
@@ -21,13 +21,26 @@
 
 (defvar ccache-wrapper-path (locate-library "ccache-wrapper.so"))
 
+(defun ccache-wrapper-environment ()
+  (format "LD_PRELOAD=%s" (expand-file-name ccache-wrapper-path)))
+
 (define-minor-mode ccache-wrapper-mode
+  "Minor mode for compilation with `ccache'."
+  :lighter " ccache"
+  :global nil
+  :group 'comilation
+  (if ccache-wrapper-mode
+      (setq-default compilation-environment (cons (ccache-wrapper-environment)
+                                                  compilation-environment))
+    (setq-default compilation-environment (remove (ccache-wrapper-environment) compilation-environment))))
+
+(define-minor-mode global-ccache-wrapper-mode
   "Global minor mode for compilation with `ccache'."
   :global t
   :group 'compilation
-  (if ccache-wrapper-mode
-      (add-to-list 'compilation-environment (format "LD_PRELOAD=%s" (expand-file-name ccache-wrapper-path)))
-    (setq compilation-environment (delete (format "LD_PRELOAD=%s" (expand-file-name ccache-wrapper-path)) compilation-environment))))
+  (if global-ccache-wrapper-mode
+      (add-hook 'compilation-mode-hook #'ccache-wrapper-mode)
+    (remove-hook 'compilation-mode-hook #'ccache-wrapper-mode)))
 
 (provide 'ccache-wrapper)
 ;;; ccache-wrapper.el ends here
